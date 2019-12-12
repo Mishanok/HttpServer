b9830a913154c0e6b8f5e026975ff0c29db74cab
Mykhailo Mykytiuk <mykhailo.mykytiuk@globallogic.com>
2019-12-12 12:53:36 +0200

    Text version of response

--- responser.c
+++ responser.c
@@ -6,13 +6,6 @@
 #include <unistd.h>
 
 #define HEADERLENGTH 12
-#define BUFFLEN 1024
-#define HEADER_OK_LEN 150
-
-enum ContentType type;
-int sizeOfType;
-static char * response = NULL;
-void* fileContent = NULL;
 
 void createResponseFAIL(enum StatusCode code, char * message)
 {
@@ -23,32 +16,44 @@ void createResponseFAIL(enum StatusCode code, char * message)
 
 void createResponseOK(char * filepath)
 {
-	char* filePathCopy = (char *)malloc(sizeof(char) * strlen(filepath));
-	strcpy(filePathCopy, filepath);
-	const char * contentType= getContentType(filePathCopy);
+	FILE * file = fopen(filepath, "r");
+
+	char buffer[1000];
+
+	char c = fgetc(file);
+	int i =0;
+	int dotInxex;
+	while(c != EOF)
+	{
+	    buffer[i] = c;
+	    c = fgetc(file);
+	    i++;
+	}
+	buffer[i+1] = '\0';
+
+	const char * contentType= getContentType(filepath);
 	if(contentType == NULL)
 	{
 	    createResponseFAIL(NotFound, "NOT FOUND");
 	    return;
 	}
 
-	void * buffer = getFileContent(filepath, type);
-
-	int contentLength = sizeof(buffer);
-	char header[HEADER_OK_LEN];
-	int bytes = snprintf(header,HEADER_OK_LEN, "HTTP/1.1 200 OK\n");
-	bytes+= snprintf(header + bytes, HEADER_OK_LEN, "Content-Type: %s\nContent-Length: %d\n\n\0", contentType, contentLength);
+	int contentLength = strlen(buffer);
+	char header[150];
+	int bytes = snprintf(header,150, "HTTP/1.1 200 OK\n");
+	bytes+= snprintf(header + bytes, 150, "Content-Type: %s\nContent-Length: %d\n\n\0", contentType, contentLength);
 
 	response = (char *)malloc(sizeof(char) * (contentLength + strlen(header)));
-	fileContent = buffer;
 
-	bytes = snprintf(response, HEADER_OK_LEN, "%s", header);
+	bytes = snprintf(response, 150, "%s", header);
+	bytes += snprintf(response + bytes, 1000, "%s", buffer);
 }
 
 static const char * getContentType(char * filepath)
 {
 	char * token = strtok(filepath, ".");
 	char * buffer = (char *)malloc(sizeof(char) * 100);
+	enum ContentType type;
 	do
 	{
 	    strcpy(buffer, token);
@@ -64,33 +69,28 @@ static const char * getContentType(char * filepath)
 	else if(strcmp(buffer, "png") == 0)
 	    type  = ImagePNG;
 	else
-	    type  = Text;
+	    type  = Unknown;
 
 	return getStrFromType(type);
 }
 
-static const char * getStrFromType(enum ContentType contType)
+static const char * getStrFromType(enum ContentType type)
 {
 	switch(type)
 	{
 	case Text:
-	    sizeOfType = sizeof(char);
 	    return "text/plain; charset=UTF-8";
 	    break;
 	case HTML:
-	    sizeOfType = sizeof(char);
 	    return "text/html; charset=UTF-8";
 	    break;
 	case ImageJPEG:
-	    sizeOfType = 1;
 	    return "image/jpeg";
 	    break;
 	case ImagePNG:
-	    sizeOfType = 1;
 	    return "image/png";
 	    break;
 	default:
-	    sizeOfType = 0;
 	    return NULL;
 	    break;
 	}
@@ -115,26 +115,8 @@ int sendResponse(int* socket, enum StatusCode status, char* filepath)
 	}
 
 	write(*socket, response, strlen(response));
-	if(fileContent != NULL)
-	    write(*socket, fileContent, sizeof(fileContent));
 	printf("-------------------------REQUEST-------------------\n");
 	printf("%s", response);
 	free(response);
 	return 0;
-}
-
-static void* getFileContent(char* filepath, enum ContentType contentType)
-{
-	FILE * file = fopen(filepath, "rb");
-
-	long size = 0;
-
-	fseek(file, 0L, SEEK_END);
-	size = ftell(file);
-
-	void * buffer = malloc(size);
-	fread(buffer, sizeOfType, size / sizeOfType, file);
-	fclose(file);
-
-	return buffer;
 }
--- responser.h
+++ responser.h
@@ -5,11 +5,13 @@ enum StatusCode {NotFound = 404, OK = 200, BadRequest = 400, NotImplemented = 50
 
 enum ContentType {Text = 10, HTML = 11, ImageJPEG = 12, ImagePNG = 13, Unknown = 0};
 
+static char * response;
+
 static void createResponseFAIL(enum StatusCode code, char* message);
 static void createResponseOK(char * filepath);
 int sendResponse(int * socket, enum StatusCode status, char * filepath);
 
 static const char* getContentType(char* filepath);
 static const char* getStrFromType(enum ContentType type);
-static void * getFileContent(char* filepath, enum ContentType type);
+static void * getFileContent(char* filepath);
 #endif
